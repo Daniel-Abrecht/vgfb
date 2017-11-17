16d46ff756ab7f9e55120055822872a152b11500
Daniel Abrecht <public@danielabrecht.ch>
2017-11-17 20:44:29 +0000

    Fixed a minor mistake

--- driver/vg.h
+++ driver/vg.h
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2017 Daniel Patrick Abrecht
+ * 
+ * This module is dual licensed under the MIT License and
+ * the GNU General Public License v2.0
+ */
+
 #ifndef VG_H
 #define VG_H
 
@@ -5,6 +12,6 @@
 
 #define VG_MAGIC 0x5647
 
-#define IOCTL_WAIT_RESIZE_DONE _IOR(VG_MAGIC, 0, unsigned long*)
+#define IOCTL_WAIT_RESIZE_DONE _IO(VG_MAGIC, 0)
 
 #endif
